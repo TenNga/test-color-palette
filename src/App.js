351667d4c5ea0e03a1876b36148ec6b443b35eb8
tennga <tenngawang6080@gmail.com>
2020-07-21 23:55:45 -0400

    testing react-palette

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 
 import ImageUploader from 'react-images-upload';
+
 import './App.css';
 
 class App extends Component{
@@ -18,7 +19,11 @@ class App extends Component{
   
 
   render(){
-    console.log("Picture: ", this.state.picture);
+    console.log("Picture: ", this.state.pictures[0]);
+    if(this.state.pictures.length > 0){
+      const path = URL.createObjectURL(this.state.pictures[0])
+      console.log("URL: ", path)
+    }
     return (
       <div className="App">
         <ImageUploader
@@ -27,7 +32,11 @@ class App extends Component{
                   onChange={this.onDrop}
                   imgExtension={['.jpg', '.gif', '.png', '.gif']}
                   maxFileSize={5242880}
-              />
+                  withPreview={true}
+        />
+        {this.state.pictures.length > 0?
+         <img src={URL.createObjectURL(this.state.pictures[0])} /> 
+         : null}
       </div>
     );
   }
